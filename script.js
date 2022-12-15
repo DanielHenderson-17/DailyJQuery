bf393b49aca2d0e55aea35eb76352a4bf3444c5f
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2022-12-14 19:36:15 -0600

    12/14/2022

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // Fill in the blanks to call the show() function for the element with the id="menu".
 // 12/13/2022
 $("#menu").show()
+
+// Select all elements that are direct children of div elements.
+$("div >*")
